f1b9c0d759d2fbc4dbcbd74b14efa528bc88c0d4
nasirAhmedPR <nasir.ahmed@parkingrhino.com>
2020-03-05 01:13:27 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express()
+const path = require('path');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const layout = require('express-ejs-layouts');
+
+
+// middlewares
+app.use(cors())
+app.use(layout)
+app.use(bodyParser.json())
+
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'assets')));
+
+// API routes
+
+app.use('/', require('./routers/index'))
+
+
+app.listen(3000,function(e){
+    console.log("Listening on 3000")
+})
