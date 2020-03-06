1c2942d8b05e908cafeb55667536f02a8f708994
nasirAhmedPR <nasir.ahmed@parkingrhino.com>
2020-03-06 09:40:31 +0530

    Removed sharp

--- controllers/GetImageController.js
+++ controllers/GetImageController.js
@@ -1,27 +1,17 @@
-const config = require("../config/config");
-const imageMetaModel = require("../models/ImageMetaModel");
+const config = require('../config/config');
+const imageMetaModel = require('../models/ImageMetaModel');
 
-module.exports = function(fetchType) {
-  return new Promise((resolve, reject) => {
-    imageMetaModel.find(
-      {},
-      null,
-      { sort: { date: -1 }, limit: config.MAX_FETCH_LIMIT },
-      function(err, docs) {
-        if (err) {
-          console.log(err);
-          reject([]);
-        }
-
-        let filenames = [];
-        for (let file of docs) {
-          filenames.push({
-            thumb:  config.IP + "/thumb" + file.filename,
-            larger: config.IP + "/larger" + file.filename
-          });
-        }
-        resolve(filenames);
-      }
-    );
-  });
-};
+module.exports = function(fetchType){
+    return new Promise((resolve, reject)=>{
+        imageMetaModel.find({}, null, {sort: {date: -1}, limit: config.MAX_FETCH_LIMIT}, function(err, docs) {
+            if(err) {console.log(err); reject([])}
+    
+            let filenames = []
+            for(let file of docs){
+                filenames.push(config.IP+"/"+file.filename)
+            }
+            resolve(filenames)
+        });
+    })
+    
+}
--- routers/upload.js
+++ routers/upload.js
@@ -10,18 +10,18 @@ var calculationService = require("../services/calculationService");
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
     cb(null, "uploads/");
+  },
+  filename: function(req, file, cb) {
+    let date = new Date().getTime().toString();
+    let fileName = date + file.originalname;
+    let obj = new model({
+      filename: fileName
+    });
+    obj.save(function(err, _) {
+      if (err) cb(null, false);
+      cb(null, fileName);
+    });
   }
-  //   filename: function(req, file, cb) {
-  //     let date = new Date().getTime().toString();
-  //     let fileName = date + file.originalname;
-  //     let obj = new model({
-  //       filename: fileName
-  //     });
-  //     obj.save(function(err, _) {
-  //       if (err) cb(null, false);
-  //       cb(null, fileName);
-  //     });
-  //   }
 });
 
 const fileFilter = (req, file, cb) => {
@@ -44,44 +44,14 @@ const upload = multer({
   fileFilter: fileFilter
 }).array("productImage", config.MAX_UPLOAD_LIMIT);
 
-router.post("/", upload, async function(req, res) {
-  await Promise.all(
-    req.files.map(async file => {
-      let dimensions = imageSize(file.path);
-      dimensions = calculationService(dimensions.height, dimensions.width);
-      console.log(dimensions);
-      sharp(file.path)
-        .resize(dimensions.l1, dimensions.w1)
-        .toFile("uploads/larger" + file.originalname, function(err) {
-          if (err) {
-            console.log(err);
-            res.status(500).send(err);
-          }
-          sharp(file.path)
-            .resize(dimensions.l2, dimensions.w2)
-            .toFile("uploads/thumb" + file.originalname, function(err) {
-              if (err) {
-                console.log(err);
-                res.status(500).send(err);
-              } else {
-                //   console.log(file)
-                let date = new Date().getTime().toString();
-                let obj = new model({
-                  filename: date+file.originalname
-                });
-                obj.save(function(err, _) {
-                  if (err) {
-                    console.log(err);
-                    res.status(500).send(err);
-                  } else {
-                    res.send("He");
-                  }
-                });
-              }
-            });
-        });
-    })
-  );
+router.post("/", async function(req, res) {
+    upload(req, res, function(err){
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.send("Upload success")
+        }
+    });
 });
 
 router.get("/", function(req, res) {
