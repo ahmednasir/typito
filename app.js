9fd8d48e403f7ff76fd4e5e4423bc8b07bfb1d10
nasirAhmedPR <nasir.ahmed@parkingrhino.com>
2020-03-07 10:57:48 +0530

    Implemented drag and drop upload

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ const path = require('path');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
+var upload = require('express-fileupload');
 const layout = require('express-ejs-layouts');
+
 require('dotenv').config()
 
 
 // middlewares
 // app.use(cors())
 app.use(layout)
-app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(upload())
+
 
 // mongodb
 mongoose.connect(process.env.DB_STRING,{ useNewUrlParser: true, useUnifiedTopology: true },(err)=>{
--- routers/testUpload.js
+++ routers/testUpload.js
@@ -1,9 +1,41 @@
 const express = require('express');
 var router = express.Router()
+var path = require('path');
+var upload = require('express-fileupload');
+
 
 
 router.get('/', function(req,res){
-    res.render('testUpload');
+    res.render('testUpload',{
+        layout: false
+    });
+})
+
+router.post('/', function(request, response){
+    var images = new Array();
+    if(request.files) {
+        var arr;
+        if(Array.isArray(request.files.filesfld)) {
+            arr = request.files.filesfld;
+        }
+        else {
+            arr = new Array(1);
+            arr[0] = request.files.filesfld;
+        }
+        for(var i = 0; i < arr.length; i++) {
+            var file = arr[i];
+            if(file.mimetype.substring(0,5).toLowerCase() == "image") {
+                images[i] = "/" + file.name;
+                file.mv("upload" + images[i], function (err) {
+                    if(err) {
+                        console.log(err);
+                    }
+                });
+            }
+        }
+    }
+    // give the server a second to write the files
+    setTimeout(function(){response.json(images);}, 1000);
 })
 
 module.exports = router
