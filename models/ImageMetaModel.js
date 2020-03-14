bfc175cfa219f038913ef26a4926c65f8d809e92
nasirAhmedPR <nasir.ahmed@parkingrhino.com>
2020-03-14 13:33:51 +0530

    Pointed to PROD collection

--- models/ImageMetaModel.js
+++ models/ImageMetaModel.js
@@ -25,4 +25,4 @@ var imageMeta = mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model("ImageMetaData", imageMeta, "ImageMetaDataV2");
+module.exports = mongoose.model("ImageMetaData", imageMeta, "ImageMetaData");
